e0fcac3d6c3e78db67e2be153dfe7e69ce370121
AnnBenavides <andrea.benavidesj@gmail.com>
2017-12-18 16:54:36 -0300

    18122017

--- Diagramas/scraperLogin.py
+++ Diagramas/scraperLogin.py
@@ -1,13 +1,30 @@
-import request
-from lxml import html
 
-payload = {
-		"username": "<USER NAME>"
-		"password": "<PASSWORD>"
-}
 
-session_requests = requests.session()
-login_url = "https://clientes.nic.cl"
-result session_request.get(login_url)
+#######
+import urllib
+import urllib2
+import cookielib
+import datetime
 
-tree = html. fromstring(result.text)
+cj = cookielib.CookieJar()
+browser = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+def open(url, postdata=None):
+    if postdata is not None:
+        postdata = urllib.urlencode(postdata)
+    return browser.open(url, postdata).read()
+
+USER = 'andrea.benavidesj@gmail.com'
+PASS = str(input("password: "))
+URL = 'https://clientes.nic.cl/registrar/logon.do;jsessionid=FF260AC82C2C4F8D84D1A1DEA2940DA5.worker2'
+now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+open(URL)
+
+POST = {'j_username': USER,
+        'password': PASS
+        }
+
+loggedIn = open(URL, POST)
+
+print(loggedIn)
