86060a4e963c576073d9547469170f306faa2a40
AnnBenavides <andrea.benavidesj@gmail.com>
2017-12-26 13:08:16 -0300

    26122018

--- PerformanceTest/src/registry/WhoIs.java
+++ PerformanceTest/src/registry/WhoIs.java
@@ -3,9 +3,14 @@ package registry;
 import java.io.IOException;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlRadioButtonInput;
@@ -17,9 +22,12 @@ public class WhoIs {
 			"antigua","farmacia","azucar","abajo","bolso",
 			"cama","casa","vendedor","raton","fruta",
 			"triangulo","flor","bota","ruta","jardin"};
+	private int simpleLimit = 1;
 	private String[] testNumberSites = {"1","2","34","5","01"};
+	private int numberLimit = 1;
 	private String[] testSpecialSites = {"ñandú", "jardín", 
 			"corazón", "sillón", "rápido", "teléfono"};
+	private int specialLimit = 1;
 	private String[] filterValues = {"exacta",
 			"contenga","comience","termine"};
 	
@@ -32,7 +40,7 @@ public class WhoIs {
 	 * dados por la lista filterValues
 	 * 
 	 * retorna la pagina luego de la busqueda**/
-	public HtmlPage submitForm(String testCase,String methodValue) throws Exception{
+	private HtmlPage submitForm(String testCase,String methodValue) throws Exception{
 		try (final WebClient webClient = new WebClient()) {
 			final HtmlPage page = webClient.getPage("http://www.nic.cl/registry/Whois.do");
 	        final HtmlForm form = page.getFormByName("buscarDominioForm");
@@ -46,41 +54,57 @@ public class WhoIs {
 	        return button.click();
 		}
 	}
+		
+	private void verifyDomNotFound(HtmlPage page){
+		final DomElement button = page.getElementById("submitButton");
+		String btn = button.getTextContent();
+		if (btn.contains("Inscribir")){
+			String href = button.getAttribute("onclick");
+			String nextUrl = "https://clientes.nic.cl/registrar/agregarDominio.do?";
+			assertTrue(href.contains(nextUrl));
+		} else { 
+			assertTrue(false);
+		}
+	}
 	
+	private boolean domNotFound(HtmlPage page){
+		final List<HtmlElement> inTable= page.getByXPath("//table[@class='tablabusqueda']/tbody/tr/td[1]");
+		final HtmlElement elem = inTable.get(0);
+		String response = elem.asText();
+		if (response.contains("no arrojó resultados")){
+			return true;
+		} else {return false;}
+	}
+	
+//////////////// TEST con filtro "exacta" ////////////////////
 	/**Metodo para verificar el resultado de una busqueda
 	 * 
+	 * page es la pagina generada post submit
+	 * 
+	 * word es la palabra buscada
 	 * **/
-	
-	private void verifyResults(HtmlPage page){// TODO
-		
+	private void verifyExactaResults(HtmlPage page, String word){// TODO
+		if (!domNotFound(page)){
+			// TODO verificar dato mostrado con word.cl
+			
+		} else {
+			// dominio no encontrado
+			verifyDomNotFound(page);
+		}
 	}
 	
-	private void failure(){// TODO
-	
-	}
-	private void success(){// TODO
-		
-	}
-		
-
 	@Test
 	public void exactaSimpleTest(){
 		HtmlPage page;
-		int limit = 10;
-		for (int i = 0 ; i < limit; i++){
+		int limit = 1;
+		for (int i = 0 ; i < simpleLimit; i++){
 			try {
-				System.out.println("Testing "+(i+1)+" of "+limit+" sites...");
 				page = submitForm(testSimpleSites[i],filterValues[0]);
 				System.out.println("Verificando contenido...");
-				
+				verifyExactaResults(page, testSimpleSites[i]);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}		
 	}
-	
-	
-	
-	
-
 }
