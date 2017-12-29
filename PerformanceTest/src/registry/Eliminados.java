6c652c5aa745d03b47e13e3364982f3228b84fd8
AnnBenavides <andrea.benavidesj@gmail.com>
2017-12-29 11:09:26 -0300

    29122017

--- PerformanceTest/src/registry/Eliminados.java
+++ PerformanceTest/src/registry/Eliminados.java
@@ -2,46 +2,80 @@ package registry;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
 import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 
 /*Test unitario para /registry/Eliminados.do*/
 public class Eliminados {
-	/**Validar URL**/
-	@Test
-	public void openLastDay() throws Exception{
+	
+	private HtmlPage openPage(String filter) throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        final HtmlPage page = webClient.getPage("http://www.nic.cl/registry/Eliminados.do?t=1d");
-	        assertEquals("NIC Chile", page.getTitleText());
+			String url ="http://www.nic.cl/registry/Eliminados.do?t=1"+filter;
+	        HtmlPage page = webClient.getPage(url);
+	        assertTrue(page.isHtmlPage());
+	        return page;
 	    }
 	}
 	
-	/**Verificar el contenido**/
+	private void verifyRow(HtmlElement element){
+		List<HtmlElement> divs = element.getElementsByTagName("div");
+		assertTrue(!divs.isEmpty());
+		
+		String text = divs.get(0).asText();
+		//System.out.println(text);
+		assertTrue(text.endsWith(".cl"));
+						
+		List<HtmlElement> buttons = element.getElementsByTagName("button");
+		assertTrue(!buttons.isEmpty());
+		
+		String url = "https://clientes.nic.cl/registrar/agregarDominio.do?d=";
+		String href = buttons.get(0).getAttribute("onclick");
+		assertTrue(href.contains(url));
+	}
+		
+	private void verifyResults(HtmlPage page){
+		try {
+			final List<HtmlTable> table = page.getByXPath("//table[@class='tablabusqueda']");
+			assertTrue(!table.isEmpty());
+			List <HtmlTableRow> rows = table.get(0).getRows();
+			for (int i = 1; i < rows.size() ; i++){
+				verifyRow(rows.get(i));				
+			}
+			HtmlElement title = rows.get(0);
+			String deletedSites = ""+(rows.size()-1);
+			String[] count = title.asText().split(" ");
+			assertTrue(count[0].equals(deletedSites));			
+		} catch (Exception e){
+			System.out.println(e); 
+			assertTrue(false);
+		}
+	}
+	
+	/**Validar URL**/
 	@Test
 	public void lastDay() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        final HtmlPage page = webClient.getPage("http://www.nic.cl/registry/Eliminados.do?t=1d");
-	        final List<HtmlTable> table= page.getByXPath("//table[@class='tablabusqueda']");
-	        assertFalse(table.isEmpty()); // existe tabla de eliminados class="tablabusqueda"
-	        HtmlTable theTable = table.get(0);
-	        List<HtmlTableRow> rows = theTable.getRows();
-	        int numberOfRows = rows.size();
-	        assertTrue(numberOfRows > 1); // contiene al menos un elemento
+			HtmlPage page = this.openPage("d");
+			this.verifyResults(page);
 	    }
 	}
 	
 	/**Validar URL**/
 	@Test
-	public void openlastWeek() throws Exception{
+	public void lastWeek() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        final HtmlPage page = webClient.getPage("http://www.nic.cl/registry/Eliminados.do?t=1w");
-	        assertEquals("NIC Chile", page.getTitleText());
+			HtmlPage page = this.openPage("w");
+			this.verifyResults(page);
 	    }
 	}
 
--- PerformanceTest/src/registry/Ultimos.java
+++ PerformanceTest/src/registry/Ultimos.java
@@ -1,30 +1,58 @@
 package registry;
 
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Ultimos {
-
+	
+	private HtmlPage openPage(String filter) throws Exception{
+		try (final WebClient webClient = new WebClient()) {
+			String url ="http://www.nic.cl/registry/Ultimos.do?t=1"+filter;
+	        HtmlPage page = webClient.getPage(url);
+	        assertTrue(page.isHtmlPage());
+	        return page;
+	    }
+	}
+	private Date parseDate(String stringDate){
+		SimpleDateFormat formatter = new SimpleDateFormat(""); 
+		Date date = null;
+		try {
+			date = formatter.parse(stringDate);
+			assertTrue(true);
+		} catch (ParseException e){
+			assertTrue(false);
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
 	/**Validar URLs**/
 	@Test
 	public void openHour() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        webClient.getPage("http://www.nic.cl/registry/Ultimos.do?t=1h");
+	        HtmlPage page = this.openPage("h");
 	    }
 	}
 	
 	@Test
 	public void openDay() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        webClient.getPage("http://www.nic.cl/registry/Ultimos.do?t=1d");
+			HtmlPage page = this.openPage("d");
 	    }
 	}
 	
 	@Test
 	public void openWeek() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        webClient.getPage("http://www.nic.cl/registry/Ultimos.do?t=1w");
+			HtmlPage page = this.openPage("w");
 	    }
 	}
 	
@@ -32,7 +60,7 @@ public class Ultimos {
 	 * Por alguna razon este test demora mucho**/
 	public void openMonth() throws Exception{
 		try (final WebClient webClient = new WebClient()) {
-	        webClient.getPage("http://www.nic.cl/registry/Ultimos.do?t=1m");
+			HtmlPage page = this.openPage("m");
 	    }
 	}
 	
